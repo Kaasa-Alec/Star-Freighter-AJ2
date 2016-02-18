4320c8d5e76650d6ceb2c37d58817fe7b1e0e129
JeffJones <JeffJones@jeffreys-mbp.attlocal.net>
2016-02-17 22:08:06 -0600

    2/17/16 Commit fixed modifications that were in error by Jeff Jones

--- Star-Freighter-AJ/src/byui/cit260/starfreighteraj/model/ShipUpgrade.java
+++ Star-Freighter-AJ/src/byui/cit260/starfreighteraj/model/ShipUpgrade.java
@@ -43,72 +43,17 @@ public class ShipUpgrade implements Serializable{
         return blocked;
     }
 
-    public void setBlocked(boolean blocked) {
-        this.blocked = blocked;
+    public void setBlocked(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public boolean isUpgradeAvailable() {
-        return upgradeAvailable;
+    public void setUpgradeAvailable(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void setUpgradeAvailable(boolean upgradeAvailable) {
-        this.upgradeAvailable = upgradeAvailable;
+    public void setUpgradeType(String stronger_lasers) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+  }
 
-    public String getUpgradeType() {
-        return upgradeType;
-    }
 
-    public void setUpgradeType(String upgradeType) {
-        this.upgradeType = upgradeType;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 71 * hash + Objects.hashCode(this.description);
-        hash = 71 * hash + (int) (Double.doubleToLongBits(this.travelTime) ^ (Double.doubleToLongBits(this.travelTime) >>> 32));
-        hash = 71 * hash + (this.blocked ? 1 : 0);
-        hash = 71 * hash + (this.upgradeAvailable ? 1 : 0);
-        hash = 71 * hash + Objects.hashCode(this.upgradeType);
-        return hash;
-    }
-
-    @Override
-    public String toString() {
-        return "ShipUpgrade{" + "description=" + description + ", travelTime=" + travelTime + ", blocked=" + blocked + ", upgradeAvailable=" + upgradeAvailable + ", upgradeType=" + upgradeType + '}';
-    }
-    
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final ShipUpgrade other = (ShipUpgrade) obj;
-        if (Double.doubleToLongBits(this.travelTime) != Double.doubleToLongBits(other.travelTime)) {
-            return false;
-        }
-        if (this.blocked != other.blocked) {
-            return false;
-        }
-        if (this.upgradeAvailable != other.upgradeAvailable) {
-            return false;
-        }
-        if (!Objects.equals(this.description, other.description)) {
-            return false;
-        }
-        if (!Objects.equals(this.upgradeType, other.upgradeType)) {
-            return false;
-        }
-        return true;
-    }
-    
-    
-    
-}
---
+++ Star-Freighter-AJ/test/byui/cit260/starfreighteraj/control/ControlSuite.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.starfreighteraj.control;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author JeffJones
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({byui.cit260.starfreighteraj.control.GameControlTest.class, byui.cit260.starfreighteraj.control.ShipUpgradeControlTest.class, byui.cit260.starfreighteraj.control.InventoryControlTest.class, byui.cit260.starfreighteraj.control.MapControlTest.class, byui.cit260.starfreighteraj.control.ShopControlTest.class})
+public class ControlSuite {
+    
+}
