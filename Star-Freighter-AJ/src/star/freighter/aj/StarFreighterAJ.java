6658cb7fbac410e8a785a2b75bc0f6cebbdcd38e
JeffJones <JeffJones@jeffreys-mbp.attlocal.net>
2016-02-19 21:02:34 -0600

    2/19/16 Lesson 6 View Classes by Jeff Jones

--- Star-Freighter-AJ/src/star/freighter/aj/StarFreighterAJ.java
+++ Star-Freighter-AJ/src/star/freighter/aj/StarFreighterAJ.java
@@ -6,66 +6,50 @@
 package star.freighter.aj;
 
 import byui.cit260.starfreighteraj.model.Game;
-import byui.cit260.starfreighteraj.model.Location;
-import byui.cit260.starfreighteraj.model.Map;
 import byui.cit260.starfreighteraj.model.Player;
-import byui.cit260.starfreighteraj.model.PlayerCharacter;
-import byui.cit260.starfreighteraj.model.StarShip;
+import byui.cit260.starfreighteraj.view.StartProgramView;
 
 /**
  *
  * @author JeffJones
  */
 public class StarFreighterAJ {
+    
+    private static Game currentGame = null;
+    private static Player player = null;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Game gameOne = new Game();
-        Player playerOne = new Player();
-        Map playerOneMap = new Map();
-        PlayerCharacter mike = new PlayerCharacter();
-        Location playerLocation = new Location();
-        StarShip playerShip = new StarShip();
         
-       
-       gameOne.setTotalTime(60);
-       gameOne.setNumberOfPlayers(1);
-       playerOne.setName("Fred Flinstone");
-       playerOne.setTotalMoneyEarned(700.00);
-       playerOneMap.setRowCount(5);
-       playerOneMap.setColumnCount(22);
-       mike.setName("Captain Kirk");
-       mike.setDescription("Captain of the Ship");
-       mike.setCoordinates(6.66);
-       mike.setStatistics(1000);
-       playerLocation.setRow(5);
-       playerLocation.setColumn(10);
-       playerLocation.setExplored(true);
-       playerLocation.setAmountRemaining(70);
-       playerShip.setHull(200);
-       playerShip.setSensors(25);
-       playerShip.setCargoBays(4);
-       playerShip.setWeaponTurrets(7);
-       playerShip.setShields(300);
-       playerShip.setMaxFuelCapacity(75);
-       
-       
-       
-       String gameInfo = gameOne.toString();
-       System.out.println(gameInfo);
-       String playerInfo = playerOne.toString();
-       System.out.println(playerInfo); 
-       String mapInfo = playerOneMap.toString();
-       System.out.println(mapInfo);
-       String playercharacterInfo = mike.toString();
-       System.out.println(playercharacterInfo);
-       String locationInfo = playerLocation.toString();
-       System.out.println(locationInfo);
-       String shipInfo = playerShip.toString();
-       System.out.println(shipInfo);
-       
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.displayStartProgramView();
+        
+        
     }
-}
+
+    public static Game getCurrentGame() {
+        return currentGame;
+    }
+
+    public static void setCurrentGame(Game currentGame) {
+        StarFreighterAJ.currentGame = currentGame;
+    }
+
+    public static Player getPlayer() {
+        return player;
+    }
+
+    public static void setPlayer(Player player) {
+        StarFreighterAJ.player = player;
+    }
+    
+    
+    
+        
+        
+         
+    }
+
 
