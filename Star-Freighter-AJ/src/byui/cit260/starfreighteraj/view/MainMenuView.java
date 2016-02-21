f65b7b9de117e032cf98b16971a30783d5d57654
JeffJones <JeffJones@jeffreys-mbp.attlocal.net>
2016-02-20 19:12:23 -0600

    2/19/16 Lesson 6 View Classes by Jeff Jones

--- Star-Freighter-AJ/src/byui/cit260/starfreighteraj/view/MainMenuView.java
+++ Star-Freighter-AJ/src/byui/cit260/starfreighteraj/view/MainMenuView.java
@@ -5,14 +5,119 @@
  */
 package byui.cit260.starfreighteraj.view;
 
+import byui.cit260.starfreighteraj.control.GameControl;
+import java.util.Scanner;
+import star.freighter.aj.StarFreighterAJ;
+
+
+
 /**
  *
  * @author JeffJones
  */
-public class MainMenuView {
+public class MainMenuView 
+{
+    private String menu;
 
-    void displayMainMenuView() {
-        System.out.println("\n*** displayMenu() function called***");
+    public MainMenuView() {
+        this.menu = "\n"
+                  + "\n-------------------------------------------"
+                  + "\n| Main Menu                               |"
+                  + "\n-------------------------------------------"
+                  + "\nG - Start new game"
+                  + "\nL - Load and start saved game"
+                  + "\nH - Get help on how to play the game"
+                  + "\nS - Save game"
+                  + "\nE - Exit game"
+                  + "\n--------------------------------------------";
     }
     
+    
+    public void displayMainMenuView() {
+        
+        boolean done = false;
+        do {
+            
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("E"))
+                return;
+            
+            done = this.doAction(menuOption);
+            
+        }  while (!done);
+        
+    }
+
+    private String getMenuOption() {
+        Scanner keyboard = new Scanner(System.in);
+        String value = ""; 
+        boolean valid = false;
+        
+        while (!valid) {
+            System.out.println("\n" + this.menu);
+            
+            value = keyboard.nextLine();
+            value = value.trim();
+            
+            if (value.length() < 1) {
+                System.out.println("\nInvalid value: The value cannot be blank");
+                continue;
+            }
+            
+            break;
+        }
+        
+        return value;
+    }
+
+    public boolean doAction(String menuOption) {
+        
+        menuOption = menuOption.toUpperCase();
+        
+        switch (menuOption) {
+            case "G":
+                this.startNewGame();
+                break;
+            case "L":
+                this.startExistingGame();
+                break;
+            case "H":
+                this.displayHelpMenu();
+                break;
+            case "S":
+                this.saveGame();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+        }
+        
+        return false;
+    }
+
+    private void startNewGame() {
+        GameControl.createNewGame(StarFreighterAJ.getPlayer());
+        
+        GameMenuView gameMenu = new GameMenuView();
+        gameMenu.displayMenu();
+    }
+
+    private void startExistingGame() {
+        System.out.println("*** startExistingGame function called ***");
+
+    }
+
+    private void displayHelpMenu() {
+        System.out.println("*** displayHelpMenu function called ***");
+    }
+
+    private void saveGame() {
+        System.out.println("*** saveGame function called ***");
+    }
+
 }
+
+    
+    
+
+
+  
